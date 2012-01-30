ed50657d98ee460630f133eebd094ea2433f1720
Alex Kudlick <akudlick@plos.org>
2012-01-30 21:08:09 +0000

    adding some checks on results to test

--- src/test/java/org/ambraproject/admin/service/FlagManagementServiceTest.java
+++ src/test/java/org/ambraproject/admin/service/FlagManagementServiceTest.java
@@ -13,91 +13,120 @@
 
 package org.ambraproject.admin.service;
 
+import org.ambraproject.ApplicationException;
 import org.ambraproject.admin.AdminBaseTest;
+import org.ambraproject.annotation.service.AnnotationConverter;
+import org.ambraproject.annotation.service.Flag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.ambraproject.ApplicationException;
-import org.ambraproject.BaseTest;
-import org.ambraproject.annotation.service.AnnotationService;
-import org.topazproject.ambra.models.*;
+import org.topazproject.ambra.models.AnnotationBlob;
+import org.topazproject.ambra.models.Comment;
+import org.topazproject.ambra.models.UserAccount;
+import org.topazproject.ambra.models.UserProfile;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class FlagManagementServiceTest extends AdminBaseTest {
 
   @Autowired
-  FlagManagementService flagManagementService;
+  protected FlagManagementService flagManagementService;
 
   @Autowired
-  AnnotationService annotationService;
+  protected AnnotationConverter converter;
 
-  @DataProvider(name="listFlag")
+  @DataProvider(name = "listFlag")
   public Object[][] listFlag() throws MalformedURLException {
+    UserProfile creator = new UserProfile();
+    creator.setId(URI.create("id:test-id-for-creator"));
+    creator.setRealName("realname");
+    creator.setDisplayName("displayname");
+    dummyDataStore.store(creator);
 
-    Comment comment = new Comment();
-    comment.setId(URI.create("id://test-id"));
-    comment.setAnnotates(URI.create("id://test-annotates"));
-    comment.setContext("context");
-    comment.setCreator("testid");
-    comment.setCreated(new Date());
+    UserAccount userAccount = new UserAccount();
+    userAccount.setId(URI.create("testid2"));
+    userAccount.setProfile(creator);
+    dummyDataStore.store(userAccount);
+
+    Comment originalComment = new Comment();
+    originalComment.setId(URI.create("id://test-id"));
+    originalComment.setAnnotates(URI.create("id://test-annotates"));
+    originalComment.setContext("context");
+    originalComment.setCreated(new Date());
 
     //Set the body on the comment
-    AnnotationBlob body = new AnnotationBlob();
-    body.setId("testbodyid");
-    body.setBody("testbody".getBytes());
-    body.setCIStatement("ci statement");
-    comment.setBody(body);
+    AnnotationBlob originalCommentBody = new AnnotationBlob();
+    originalCommentBody.setId("testbodyid");
+    originalCommentBody.setBody("testbody".getBytes());
+    originalCommentBody.setCIStatement("ci statement");
+    originalComment.setBody(originalCommentBody);
 
-    dummyDataStore.store(body);
-    dummyDataStore.store(comment);
+    dummyDataStore.store(originalCommentBody);
+    dummyDataStore.store(originalComment);
 
-    Comment comment2 = new Comment();
-    comment2.setId(URI.create("id://test-id-2"));
-    comment2.setAnnotates(comment.getId());
-    comment2.setContext("context2");
-    comment2.setCreator("testid2");
-    comment2.setCreated(new Date());
+    Comment flag = new Comment();
+    flag.setId(URI.create("id://test-id-2"));
+    flag.setAnnotates(originalComment.getId());
+    flag.setCreator(creator.getId().toString());
+    flag.setContext("context2");
+    flag.setCreator("testid2");
+    flag.setCreated(new Date());
 
     //Set the body on the comment2
-    AnnotationBlob body2 = new AnnotationBlob();
-    body2.setId("testbodyid2");
-    body2.setBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?><flag reasonCode=\"resoncode\"><comment>Another reasoncode.</comment></flag>".getBytes());
-    body2.setCIStatement("ci statement-2");
-    comment2.setBody(body2);
+    AnnotationBlob flagBody = new AnnotationBlob();
+    flagBody.setId("testbodyid2");
+    flagBody.setBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?><flag reasonCode=\"resoncode\"><comment>Another reasoncode.</comment></flag>".getBytes());
+    flagBody.setCIStatement("ci statement-2");
+    flag.setBody(flagBody);
 
-    dummyDataStore.store(body2);
-    dummyDataStore.store(comment2);
+    dummyDataStore.store(flagBody);
+    dummyDataStore.store(flag);
 
-    UserProfile up = new UserProfile();
-    up.setRealName("realname");
-    up.setDisplayName("displayname");
-    dummyDataStore.store(up);
-
-    UserAccount ua = new UserAccount();
-    ua.setId(URI.create("testid2"));
-    ua.setProfile(up);
-    dummyDataStore.store(ua);
+    Map<Comment, Flag> results = new HashMap<Comment, Flag>(1);
+    results.put(originalComment, new Flag(converter.convert(flag, true, true)));
 
     return new Object[][]{
-      {comment}
+        {results}
     };
   }
 
   /**
    * Test for FlagManagementService.getFlaggedComments()
-   * @param annotationId for which flags should be fetched
+   *
+   * @param flaggedComments
    * @throws ApplicationException
    */
 
   @Test(dataProvider = "listFlag")
-  public void testGetFlaggedComments(Annotation annotationId) throws ApplicationException{
+  public void testGetFlaggedComments(Map<Comment, Flag> flaggedComments) throws ApplicationException {
     Collection<FlaggedCommentRecord> list = flagManagementService.getFlaggedComments();
     assertNotNull(list, "The list is null");
+    assertEquals(list.size(), flaggedComments.size());
+
+    for (Map.Entry<Comment, Flag> entry : flaggedComments.entrySet()) {
+      Comment originalComment = entry.getKey();
+      Flag flag = entry.getValue();
+      FlaggedCommentRecord record = null;
+      for (FlaggedCommentRecord flaggedCommentRecord : list) {
+        if (flaggedCommentRecord.getTarget().equals(originalComment.getId().toString())) {
+          record = flaggedCommentRecord;
+          break;
+        }
+      }
+      assertNotNull(record, "didn't find a matching FlaggedCommentRecord for " + originalComment);
+      assertEquals(record.getCreatorid(), flag.getCreator(), "Flagged Comment Record had incorrect creator");
+      assertEquals(record.getFlagId(), flag.getId(), "FlaggedCommentRecord had incorrect flag id");
+      assertEquals(record.getTarget(), originalComment.getId().toString(), "FlaggedCommentRecord had incorrect target annotation id");
+      assertEquals(record.getReasonCode(), flag.getReasonCode(), "FlaggedCommentRecord had incorrect reason code");
+      assertEquals(record.getFlagComment(), flag.getComment(), "FlaggedCommentRecord had incorrect flag comment");
+    }
   }
 }
