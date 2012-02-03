f3294792ca280b05a840141b66cd8654df032cd2
Alex Kudlick <akudlick@plos.org>
2012-02-03 00:14:38 +0000

    unneccessary method

--- src/test/java/org/ambraproject/admin/action/ManageFlagsActionTest.java
+++ src/test/java/org/ambraproject/admin/action/ManageFlagsActionTest.java
@@ -212,18 +212,4 @@ public class ManageFlagsActionTest extends AdminWebTest {
     }
 
   }
-
-
-  public static Calendar convertToTimezone(Date date, TimeZone tz) {
-    //Returns the number of milliseconds since January 1, 1970, 00:00:00 GMT
-    long msFromEpochGmt = date.getTime();
-    //gives you the current offset in ms from GMT at the current date
-    int offsetFromUTC = tz.getOffset(msFromEpochGmt);
-    //create a new calendar in GMT timezone, set to this date and add the offset
-    Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-    gmtCal.setTime(date);
-    gmtCal.add(Calendar.MILLISECOND, offsetFromUTC);
-    return gmtCal;
-  }
-
 }
